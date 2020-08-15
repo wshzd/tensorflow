f2db2c10c2b0cf7b8651af6e6e0a8d2db74b4eea
wshzd <w-s-h-z-d@163.com>
2020-08-15 20:37:55 +0800

    Update Auto_Gradient.py

--- tf2.0/Auto_Gradient.py
+++ tf2.0/Auto_Gradient.py
@@ -16,10 +16,25 @@ print(y, y_grad)
 # tf.Tensor(6.0, shape=(), dtype=float32)
 
 
+# 在机器学习中，更加常见的是对多元函数求偏导数，以及对向量或矩阵的求导。这些对于 TensorFlow 也不在话下。
+# 以下代码展示了如何使用 tf.GradientTape() 计算函数 L(w, b) = ||Xw + b - y||^2 在 w = (1, 2)^T, b = 1 时分别对 w, b 的偏导数。
 
+X = tf.constant([[1., 2.], [3., 4.]])
+y = tf.constant([[1.], [2.]])
+w = tf.Variable(initial_value=[[1.], [2.]])
+b = tf.Variable(initial_value=1.)
+with tf.GradientTape() as tape:
+    L = tf.reduce_sum(tf.square(tf.matmul(X, w) + b - y))
+w_grad, b_grad = tape.gradient(L, [w, b])        # 计算L(w, b)关于w, b的偏导数
+print(L, w_grad, b_grad)
 
+# 输出：
+# tf.Tensor(125.0, shape=(), dtype=float32)
+# tf.Tensor(
+# [[ 70.]
+# [100.]], shape=(2, 1), dtype=float32)
+# tf.Tensor(30.0, shape=(), dtype=float32)
 
-
-
-
+# 这里， tf.square() 操作代表对输入张量的每一个元素求平方，不改变张量形状。 tf.reduce_sum() 操作代表对输入张量的所有元素求和，输出一个形状为空的纯量张量（可以通过 axis 参数来指定求和的维度，不指定则默认对所有元素求和）。
+# TensorFlow 中有大量的张量操作 API，包括数学运算、张量形状操作（如 tf.reshape()）、切片和连接（如 tf.concat()）等多种类型
 
