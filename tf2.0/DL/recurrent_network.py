b8588ab4cb5aa52b30393374b4f37511d6f5277b
wshzd <w-s-h-z-d@163.com>
2020-09-22 19:36:04 +0800

    Add files via upload

---
+++ tf2.0/DL/recurrent_network.py
@@ -0,0 +1,114 @@
+# Import TensorFlow v2.
+import tensorflow as tf
+from tensorflow.keras import Model, layers
+import numpy as np
+
+# MNIST dataset parameters.
+num_classes = 10  # total classes (0-9 digits).
+num_features = 784  # data features (img shape: 28*28).
+
+# Training Parameters
+learning_rate = 0.001
+training_steps = 1000
+batch_size = 32
+display_step = 100
+
+# Network Parameters
+# MNIST image shape is 28*28px, we will then handle 28 sequences of 28 timesteps for every sample.
+num_input = 28  # number of sequences.
+timesteps = 28  # timesteps.
+num_units = 32  # number of neurons for the LSTM layer.
+
+# Prepare MNIST data.
+from tensorflow.keras.datasets import mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# Convert to float32.
+x_train, x_test = np.array(x_train, np.float32), np.array(x_test, np.float32)
+# Flatten images to 1-D vector of 784 features (28*28).
+x_train, x_test = x_train.reshape([-1, 28, 28]), x_test.reshape([-1, num_features])
+# Normalize images value from [0, 255] to [0, 1].
+x_train, x_test = x_train / 255., x_test / 255.
+
+# Use tf.data API to shuffle and batch data.
+train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+train_data = train_data.repeat().shuffle(5000).batch(batch_size).prefetch(1)
+
+
+# Create LSTM Model.
+class LSTM(Model):
+    # Set layers.
+    def __init__(self):
+        super(LSTM, self).__init__()
+        # RNN (LSTM) hidden layer.
+        self.lstm_layer = layers.LSTM(units=num_units)
+        self.out = layers.Dense(num_classes)
+
+    # Set forward pass.
+    def call(self, x, is_training=False):
+        # LSTM layer.
+        x = self.lstm_layer(x)
+        # Output layer (num_classes).
+        x = self.out(x)
+        if not is_training:
+            # tf cross entropy expect logits without softmax, so only
+            # apply softmax when not training.
+            x = tf.nn.softmax(x)
+        return x
+
+
+# Build LSTM model.
+lstm_net = LSTM()
+
+
+# Cross-Entropy Loss.
+# Note that this will apply 'softmax' to the logits.
+def cross_entropy_loss(x, y):
+    # Convert labels to int 64 for tf cross-entropy function.
+    y = tf.cast(y, tf.int64)
+    # Apply softmax to logits and compute cross-entropy.
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=x)
+    # Average loss across the batch.
+    return tf.reduce_mean(loss)
+
+
+# Accuracy metric.
+def accuracy(y_pred, y_true):
+    # Predicted class is the index of highest score in prediction vector (i.e. argmax).
+    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.cast(y_true, tf.int64))
+    return tf.reduce_mean(tf.cast(correct_prediction, tf.float32), axis=-1)
+
+
+# Adam optimizer.
+optimizer = tf.optimizers.Adam(learning_rate)
+
+
+# Optimization process.
+def run_optimization(x, y):
+    # Wrap computation inside a GradientTape for automatic differentiation.
+    with tf.GradientTape() as g:
+        # Forward pass.
+        pred = lstm_net(x, is_training=True)
+        # Compute loss.
+        loss = cross_entropy_loss(pred, y)
+
+    # Variables to update, i.e. trainable variables.
+    trainable_variables = lstm_net.trainable_variables
+
+    # Compute gradients.
+    gradients = g.gradient(loss, trainable_variables)
+
+    # Update weights following gradients.
+    optimizer.apply_gradients(zip(gradients, trainable_variables))
+
+
+# Run training for the given number of steps.
+for step, (batch_x, batch_y) in enumerate(train_data.take(training_steps), 1):
+    # Run the optimization to update W and b values.
+    run_optimization(batch_x, batch_y)
+
+    if step % display_step == 0:
+        pred = lstm_net(batch_x, is_training=True)
+        loss = cross_entropy_loss(pred, batch_y)
+        acc = accuracy(pred, batch_y)
+        print("step: %i, loss: %f, accuracy: %f" % (step, loss, acc))
